bccd556f5060a62171a66d97bfbcf3b2853429f2
WenJun <nczywq@qq.com>
2025-03-13 15:40:10 +0800

    重新配置驱动

---
+++ README.md
@@ -0,0 +1,96 @@
+# 小华单片机hc32f4a0系列cmake驱动
+## 介绍
+ - 驱动来自小华的2.2.3版本,修改了部分代码,添加了部分功能.
+ - 支持配置为`bootloader,app,normal`三种模式.
+ - 还支持是否有`rt-thread`系统.
+ - bootloader模式下,flash前128KB为bootloader,其余为app.
+ - 添加了`printf`支持浮点打印.
+ - 使用了最严格的编译选项,将所有警告视为错误,禁止编译,来养成良好的编程习惯.
+ - 当使用gcc-arm-none-eabi,11及以上版本的编译器时，编译时会出现`LOAD segment with RWX permissions 警告`,`toolchain`中可以忽略这个警告，但是忽略参数11版本以下的又不支持,所以保留了这个警告,修改链接脚本也可以消除此警告,但是同样不支持11版本以下的编译器,固没做修改.
+## 修改内容
+ - `hc32_ll_can.c`中`const static uint8_t m_au8DLC2WordSize[16U]`修改为`static const uint8_t m_au8DLC2WordSize[16U] `将`static`放在最前面,以规范代码,去除警告.
+ - 当有`rt-thread`
+   - 修改了`startup_hc32f4a0.S`中`bl main`修改为`bl entry`来支持`rt-thread`系统.
+   - 修改了`HC32F4A0xI.ld`和`HC32F4A0xG.ld`链接脚本,来支持`rt-thread`系统相关自动初始化函数,来保证不被编译器优化.
+## 使用前准备
+- [Cmake下载](https://cmake.org/download/)
+  - 安装Cmake,会自动将cmake安装到环境变量中,如果`cmake`提示未找到要手动将cmake的bin目录加入到环境变量中.
+- [MinGW下载](https://github.com/niXman/mingw-builds-binaries/releases)
+  - 安装MinGW编译器,如果是是解压类型,解压后把bin目录加入到环境变量中.
+- [gcc-arm-none-eabi下载](https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads)
+  - 安装gcc-arm-none-eabi,如果是解压型的,解压后把bin目录加入到环境变量中.
+- [pyOCD安装方法](https://github.com/pyocd/pyOCD),这是下载和高度mcu的工具,只编译的话,不需要.
+  - 还可以安装libusb,来显示更多调试信息,比如断言.
+## Cmake 配置例子
+``` cmake
+cmake_minimum_required(VERSION 3.27)
+
+# Setup compiler settings
+set(CMAKE_C_STANDARD 17)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS ON)
+
+# Define the build type
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE "Debug")
+endif()
+
+# Set the project name
+set(CMAKE_PROJECT_NAME myproject)
+
+# Driver function selection
+set(MCU_TYPE "HC32F4A0xI" CACHE STRING "Set MCU Type")
+set(BOOTLOADER OFF CACHE BOOL "Set to ON if bootloader is enabled")
+set(APP OFF CACHE BOOL "Set to ON if app is enabled")
+set(RT-THREAD OFF CACHE BOOL "Set to ON if RT-Thread is enabled")
+
+# Include toolchain file
+include("drivers/toolchain/gcc-arm-none-eabi.cmake")
+
+# Enable compile command to ease indexing with e.g. clangd
+set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
+
+# Core project settings
+project(${CMAKE_PROJECT_NAME})
+message("Build type: " ${CMAKE_BUILD_TYPE})
+
+# Enable CMake support for ASM and C languages
+enable_language(C ASM)
+
+# Create an executable object type
+add_executable(${CMAKE_PROJECT_NAME})
+# "Generate hex and bin files and associate them with the clean target."
+add_custom_command(
+    TARGET ${CMAKE_PROJECT_NAME}
+    POST_BUILD
+        COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${CMAKE_PROJECT_NAME}> ${CMAKE_PROJECT_NAME}.hex
+        COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${CMAKE_PROJECT_NAME}> ${CMAKE_PROJECT_NAME}.bin
+    BYPRODUCTS 
+        ${CMAKE_PROJECT_NAME}.hex
+        ${CMAKE_PROJECT_NAME}.bin
+)
+# Include the drivers,Drivers can be loaded using the submodule management method, which is recommended.
+add_subdirectory("drivers")
+
+# Add User source files
+file(GLOB SOURCES
+     # Add user sources here
+)
+
+# Add User headers
+set(HEADERS
+    # Add user defined library search paths
+)
+target_sources(${CMAKE_PROJECT_NAME} PRIVATE
+    "${SOURCES}"
+)
+target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE
+    "${HEADERS}"
+)
+target_compile_definitions(${CMAKE_PROJECT_NAME} PRIVATE
+    # Add user defined symbols
+)
+target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE
+    # Add user defined libraries
+)
+```
---
+++ toolchain/gcc-arm-none-eabi.cmake
@@ -0,0 +1,94 @@
+set(CMAKE_SYSTEM_NAME               Generic)
+set(CMAKE_SYSTEM_PROCESSOR          arm)
+
+set(CMAKE_C_COMPILER_ID GNU)
+set(CMAKE_CXX_COMPILER_ID GNU)
+
+# Some default GCC settings
+# arm-none-eabi- must be part of path environment
+set(TOOLCHAIN_PREFIX                arm-none-eabi-)
+
+set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
+set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_LINKER                    ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
+set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
+
+set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# MCU specific flags
+set(TARGET_FLAGS "-mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard ")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TARGET_FLAGS}")
+set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Werror -Wextra -Wpedantic -fdata-sections -ffunction-sections")
+
+set(CMAKE_C_FLAGS_DEBUG "-O0 -g3")
+set(CMAKE_C_FLAGS_RELEASE "-Os -g0")
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3")
+set(CMAKE_CXX_FLAGS_RELEASE "-Os -g0")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+
+# linker file
+set(LINKER_SCRIPT "")
+if(MCU_TYPE STREQUAL "HC32F4A0xG") # PARENT_SCOPE
+    if(BOOTLOADER)
+        if(RT-THREAD)
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/../rt-thread/linker/bootloader/HC32F4A0xG.ld" )
+        else()
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/../linker/bootloader/HC32F4A0xG.ld" )
+        endif()
+    elseif(APP)
+        if(RT-THREAD)
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../rt-thread/linker/app/HC32F4A0xG.ld" )
+        else()
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../linker/app/HC32F4A0xG.ld" )
+        endif()
+    else()
+        if(RT-THREAD)
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../rt-thread/linker/normal/HC32F4A0xG.ld" )
+        else()
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../cmsis/Device/HDSC/hc32f4xx/Source/GCC/linker/HC32F4A0xG.ld" )
+        endif()
+    endif()
+elseif(MCU_TYPE STREQUAL "HC32F4A0xI")
+    if(BOOTLOADER)
+        if(RT-THREAD)
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../rt-thread/linker/bootloader/HC32F4A0xI.ld" )
+        else()
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../linker/bootloader/HC32F4A0xI.ld" )
+       endif() 
+    elseif(APP)
+        if(RT-THREAD)
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../rt-thread/linker/app/HC32F4A0xI.ld" )
+        else()
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../linker/app/HC32F4A0xI.ld" )
+        endif()
+    else()
+        if(RT-THREAD)
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../rt-thread/linker/normal/HC32F4A0xI.ld" )
+        else()
+            set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../cmsis/Device/HDSC/hc32f4xx/Source/GCC/linker/HC32F4A0xI.ld" )
+        endif()
+    endif()
+else()
+    message(FATAL_ERROR "Please enter the MCU model.")
+endif()
+
+set(CMAKE_C_LINK_FLAGS "${TARGET_FLAGS}")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -T \"${LINKER_SCRIPT}\"")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS}  --specs=nosys.specs")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS}  -u_printf_float -u_scanf_float")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lc -lm -Wl,--end-group")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--print-memory-usage")
+# if use arm-none-eabi-gcc > 10.3 , need add -Wl,-no-warn-rwx-segments,if =10.3 ,no need add -Wl,-no-warn-rwx-segments
+# set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,-no-warn-rwx-segments")
+
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group")
